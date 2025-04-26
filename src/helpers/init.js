3b6d8806c1dd44289e7b2918943ce7e0388e9a61
jmqoeu <jmqoeu@gmail.com>
2025-04-26 16:36:14 +0800

    Update init.js

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -10,6 +10,7 @@ export function initializeParams(request, env) {
     globalThis.defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
     globalThis.userID = '914b4e56-ee51-448a-aac4-6fd71576e7e2';
     globalThis.TRPassword = 'ikojkdjg9()_*(_';
+    globalThis.proxyIP = '138.201.244.245';
     globalThis.proxyIP = proxyIPs?.length ? proxyIPs[Math.floor(Math.random() * proxyIPs?.length)] : 'bpb.yousef.isegaro.com';
     globalThis.hostName = request.headers.get('Host');
     globalThis.pathName = url.pathname;
