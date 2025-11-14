b4d3f487970f44d992c31fceced3821f6216559b
jmqoeu <jmqoeu@gmail.com>
2025-11-14 18:12:27 +0800

    Refactor init function to use fallback values

--- src/common/init.ts
+++ src/common/init.ts
@@ -109,13 +109,11 @@ export async function setSettings(request: Request, env: Env) {
 export function init(request: Request, env: Env) {
     const { pathname } = new URL(request.url);
     const { UUID, TR_PASS, FALLBACK, DOH_URL } = env;
-    const UUID = "21863d96-ba57-499e-ae07-a97b8c8aa04e";
-    const TR_PASS = "@#$%^&*(^%R#*^%#$%^&*";
 
     
     globalThis.globalConfig = {
-        userID: UUID,
-        TrPass: TR_PASS,
+        userID: UUID || "21863d96-ba57-499e-ae07-a97b8c8aa04e",
+        TrPass: TR_PASS || "@#$%^&*(^%R#*^%#$%^&*",
         //userID: '21863d96-ba57-499e-ae07-a97b8c8aa04e',
         //TrPass: '@#$%^&*(^%R#*^%#$%^&*',
         
